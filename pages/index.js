28c9f46709fd440c81b9bd27e02be5c608ce8bfa
Sahil Kumar <sahil.kumar.bce20@itbhu.ac.in>
2022-10-29 16:10:05 +0530

    resolve conflicts of error page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-
 import NavBar from "./components/NavBar/NavBar"
-import Skills from "./components/Skills/Skills"
-import Sidebar from "./components/Sidebar/Sidebar";
-
 export default function Home() {
   return (
-   <>
-    <NavBar/>
-    <Skills/>
-    {/* <Sidebar/> */}
-   
-   </>
-  )}
+    <>
+     {/* <Sidebar/> */}
+     <NavBar/>
+    </>
+  );
+}
